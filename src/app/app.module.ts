17a407f7814e2bdb77b22cdf044a9aacb18086a4
Michael Gira <michaelgira23@gmail.com>
2017-06-03 15:44:36 +0200

    Add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule } from '@angular/material';
+import 'hammerjs';
+
+import { routing } from './routes';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		NavbarComponent,
+		DashboardComponent
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		BrowserAnimationsModule,
+		MdButtonModule,
+		routing
 	],
 	providers: [],
 	bootstrap: [AppComponent]
---
+++ src/app/routes.ts
@@ -0,0 +1,22 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+	{
+		path: '',
+		redirectTo: '/dashboard',
+		pathMatch: 'full'
+	},
+	{
+		path: 'dashboard',
+		component: DashboardComponent
+	},
+	{
+		path: '**',
+		redirectTo: '/dashboard'
+	}
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
