c23d9daa4b68266736bbb7b7ca83c77641d49b24
Michael Gira <michaelgira23@gmail.com>
2017-06-07 19:40:03 +0200

    Filter search results by tag

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -13,7 +13,15 @@ export class SearchComponent implements OnInit, OnDestroy {
 
 	queryString = '';
 	origin = 'vanilla';
+	unfilteredResults: Pack[] = [];
 	results: Pack[] = [];
+	tagCount: { [tag: string]: number } = {};
+	tagFilter: any;
+	private _tagFilter: { [tag: string]: boolean } = {};
+
+	get tags() {
+		return Object.keys(this.tagCount);
+	}
 
 	searchSubject: Subject<Query>;
 	searchSubscription: Subscription;
@@ -43,10 +51,24 @@ export class SearchComponent implements OnInit, OnDestroy {
 			.subscribe(
 				results => {
 					console.log('Search Results', results);
-					this.results = results;
+					this.unfilteredResults = results;
+					this.updateFilterTags();
 				}
 			);
 		this.search();
+
+		// Refilter results if tags filter changes
+		this.tagFilter = new Proxy(this._tagFilter, {
+			get: (target, name) => {
+				return this._tagFilter[name];
+			},
+			set: (target, property, value, receiver) => {
+				console.log('set new value!');
+				this._tagFilter[property] = value;
+				this.updateFilterTags();
+				return true;
+			}
+		});
 	}
 
 	ngOnDestroy() {
@@ -58,6 +80,49 @@ export class SearchComponent implements OnInit, OnDestroy {
 			origin: this.origin,
 			string: this.queryString
 		});
+	}
+
+	updateFilterTags() {
+		// Find how many times a tag is used
+		this.tagCount = {};
+		for (const pack of this.unfilteredResults) {
+			for (const tag of pack.tags) {
+				if (typeof this.tagCount[tag] === 'undefined') {
+					this.tagCount[tag] = 0;
+				} else {
+					this.tagCount[tag]++;
+				}
+			}
+		}
+
+		// Add new tags to filter object
+		for (const tag of Object.keys(this.tagCount)) {
+			if (typeof this._tagFilter[tag] === 'undefined') {
+				this._tagFilter[tag] = true;
+			}
+		}
+
+		// If a tag no longer exists in filter, remove it
+		for (const tag of Object.keys(this._tagFilter)) {
+			if (typeof this.tagCount[tag] === 'undefined') {
+				delete this.tagFilter[tag];
+			}
+		}
+
+		// Update results
+		this.results = [];
+		for (const pack of this.unfilteredResults) {
+			if (this.filterPack(pack)) {
+				console.log('back is okay');
+				this.results.push(pack);
+			} else {
+				console.log('back is not okay');
+			}
+		}
+	}
+
+	filterPack(pack: Pack) {
+		return pack.tags.every(tag => this.tagFilter[tag]);
 	}
 
 }
